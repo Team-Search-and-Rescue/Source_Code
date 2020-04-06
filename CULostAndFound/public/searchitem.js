77d6e3929c5429188327e52fd9c93b9c050cf25b
Omar <omka7201@colorado.edu>
2020-04-06 00:01:58 -0600

    retrieval of all items from database completed, filtering still in progress

--- CULostAndFound/public/searchitem.js
+++ CULostAndFound/public/searchitem.js
@@ -3,5 +3,17 @@ const searchButton = document.getElementById('searchBttn');
 var database = firebase.database();
 
 var ref = database.ref('items');
-var data = database.ref('scores');
 
+searchButton.addEventListener('click', (e) =>{
+    e.preventDefault();
+    //console.log("Button Works!");
+    //alert("button works!")
+    console.log('testing retrival!');
+    var itemsref = firebase.database().ref("items");
+    itemsref.on('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot){
+            var childData = childSnapshot.val();
+            console.log(childData);
+        });
+    });
+})
