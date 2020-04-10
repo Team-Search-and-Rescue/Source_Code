7c53e570397d3c3d908347381cebd2628c6fbce3
Omar <omka7201@colorado.edu>
2020-04-10 04:07:29 -0600

    Completed search by each individual input except for text input. Storing queries. Searching by date. All returns from search are returned on the array results in json format. Run a sample query and check the console for example (add an item that matches query first)

--- CULostAndFound/public/searchitem.js
+++ CULostAndFound/public/searchitem.js
@@ -1,49 +1,71 @@
 const searchButton = document.getElementById('searchBttn');
+const iQueryText = document.getElementById('queryText')
 const icolor = document.getElementById('color');
 const ilocation = document.getElementById('location');
 const icategory = document.getElementById('category');
 const istatus = document.getElementById('status');
+const idate = document.getElementById('date-lost');
 const database = firebase.database();
-var ref = database.ref('items');
+const ref = database.ref('items');
+const Querydatabase = database.ref('query');
 var allItems = [];
+var results = [];
 
 
 searchButton.addEventListener('click', (e) =>{
     e.preventDefault();
-    //console.log(icolor.value);
     statusFilter = istatus.value;
     colorFilter = icolor.value;
     categoryFilter = icategory.value;
     locationFilter = ilocation.value;
+    numberOfItems = allItems.length;
+    DateFilter = idate.value;
 
-    //console.log('Searching by color');
-    
-    //console.log(allItems);
-    //console.log('filter'); 
-
-    var filterItemStatus = allItems.filter(item => {
-        return item.status === statusFilter;
+    var inputData = {
+        query : iQueryText.value,
+        status : istatus.value,
+        color : icolor.value,
+        location : ilocation.value,
+        category : icategory.value,
+        date : idate.value
+    }
+    Querydatabase.push(inputData).then(() => {
+        
     })
 
-    var filteredItemColor = allItems.filter(item => {
-        return item.color === colorFilter;
-    })
+    if (statusFilter != 'Any'){
+        var filterItemStatus = allItems.filter(item => {
+            return item.status === statusFilter;
+        })
+    }
 
-    var filteredItemCategory = allItems.filter(item => {
-        return item.category === categoryFilter;
-    })
+    if (colorFilter != 'Any'){
+        var filteredItemColor = filterItemStatus.filter(item => {
+            return item.color === colorFilter;
+        })
+    }
 
-    var filteredItemLocation = allItems.filter(item => {
-        return item.ilocation === locationFilter;
-    })
+    if (categoryFilter != 'Any'){
+        var filteredItemCategory = filteredItemColor.filter(item => {
+            return item.category === categoryFilter;
+        })
+    }
 
-    // var filteredItemDate = allItems.filter(item => {
-    //     return item.date 
+    if (locationFilter != 'Any'){
+        var filteredItemLocation = filteredItemCategory.filter(item => {
+            return item.location === locationFilter;
+        })
+    }
 
-    console.log(filterItemStatus);
-    console.log(filteredItemColor);
-    console.log(filteredItemCategory);
-    console.log(filteredItemLocation);
+    if (DateFilter != ''){
+        var filteredItemDate = filteredItemLocation.filter(item => {
+            return item.date >= DateFilter;
+        })
+    }
+
+    var results = filteredItemDate;
+
+    console.log(results);
 })
 
 
