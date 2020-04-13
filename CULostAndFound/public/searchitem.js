3a93b88c22e366a16937bb7d6c9aa5cd0bc54b4c
Omar <omka7201@colorado.edu>
2020-04-13 17:52:14 -0600

    comments for clarity

--- CULostAndFound/public/searchitem.js
+++ CULostAndFound/public/searchitem.js
@@ -34,30 +34,35 @@ searchButton.addEventListener('click', (e) =>{
         
     })
 
+    //filtering by status lost or found
     if (statusFilter != 'Any'){
         var filterItemStatus = allItems.filter(item => {
             return item.status === statusFilter;
         })
     }
 
+    //filters by search color
     if (colorFilter != 'Any'){
         var filteredItemColor = filterItemStatus.filter(item => {
             return item.color === colorFilter;
         })
     }
-
+   
+    //filter with category
     if (categoryFilter != 'Any'){
         var filteredItemCategory = filteredItemColor.filter(item => {
             return item.category === categoryFilter;
         })
     }
 
+    //filter by location
     if (locationFilter != 'Any'){
         var filteredItemLocation = filteredItemCategory.filter(item => {
             return item.location === locationFilter;
         })
     }
 
+    //filter by date
     // if (DateFilter != ''){
     //     var filteredItemDate = filteredItemLocation.filter(item => {
     //         return item.date >= DateFilter;
@@ -66,7 +71,8 @@ searchButton.addEventListener('click', (e) =>{
 
     var results = filteredItemLocation;
 
-    console.log(item);
+    console.log(results);
+    console.log(allItems);
 
 })
 
